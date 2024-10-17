f1120057e91e4c1ca0f8f393f8743409b6dbab0e
vinay2003 <vinaysharma31681@gmail.com>
2024-10-17 10:06:16 +0530

    my changes

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const container = document.querySelector('.container');
 const loginLink = document.querySelector('.SignInLink');
 const registerLink = document.querySelector('.SignUpLink');
 
-// Event listener for Sign Up link (to switch to Register form)
 registerLink.addEventListener('click', () => {
-    container.classList.add('active'); // Add the 'active' class to switch to register mode
+    container.classList.add('active');
 });
 
-// Event listener for Sign In link (to switch back to Login form)
 loginLink.addEventListener('click', () => {
-    container.classList.remove('active'); // Remove the 'active' class to switch back to login mode
+    container.classList.remove('active');
 });
